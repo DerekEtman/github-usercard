ecfad29439030d468b5d6b0aac3b2c9cbebf5048
Derek Etman <dereketman@gmail.com>
2019-08-08 13:40:47 -0700

    linked github API via Axios.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,14 @@
            https://api.github.com/users/<your name>
 */
 
+// axios.get('https://api.github.com/users/dereketman')
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -26,6 +34,8 @@
 
 const followersArray = [];
 
+
+
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -45,6 +55,12 @@ const followersArray = [];
 </div>
 
 */
+
+const cardBuilder = () => {
+  
+}
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
